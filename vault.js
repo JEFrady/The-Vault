981e40b409e2201c34300ecd0acfef741e02b942
JEFrady <jamie.e.halliday@gmail.com>
2018-07-07 17:00:15 -1000

    All tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+    const vault = {}
+    const setValue = function(key,value) {
+        if (!key) {
+            return null;
+        }
+        else {
+            vault[key]=value;
+        }
+    }
+    const getValue = function (key) {
+        if (!key || typeof vault[key] === "undefined") {
+          return null;  
+        }
+        else {
+            return vault[key];
+        }
+    }
+    return {
+        getValue: getValue,
+        setValue: setValue
+    }
 };
